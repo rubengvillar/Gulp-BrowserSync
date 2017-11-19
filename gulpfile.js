a566ec0657bad316d759fb60c8dab2d424bb632a
Ruben G. Villar <rubengvillardesign@gmail.com>
2017-11-18 21:48:05 -0300

    Se comentaron cada seccion del archivo gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
+// Uso del modo estricto
 'use strict';
-const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    pug = require('gulp-pug'),
-    babel = require('gulp-babel'),
-    browserSync = require('browser-sync').create();
-var IsProduction = getArg("--env") === "production";
 
+// importación de los modulos necesarios.
+import gulp from "gulp";
+import sass from "gulp-sass";
+import autoprefixer  from "gulp-autoprefixer";
+import pug from "gulp-pug";
+import babel from "gulp-babel";
+import browserSync from "browser-sync";
+// instanciamos la funcion create de browserSync y re asignamos en la misma variable.
+browserSync = browserSync.create();
+
+// Variable que nos ayudara a definira si le pasamos a la linea de comandos si compilamos en modo producción.
+let IsProduction = getArg("--env") === "production";
+
+// Directorios con los que vamos a trabajar.
 let dir = {
     src : './src',
     build : './build'
 },
+
+// Definimos las opciones de cada modulo
 options = {    
     pug : {
         pretty: IsProduction ? false : true,
@@ -54,12 +64,14 @@ options = {
     }
 };
 
+// Tarea encargada de preprocesar PUG
 gulp.task('pug',() =>{
     gulp.src(`${dir.src}/pug/*.pug`)
         .pipe(pug(options.pug))
         .pipe(gulp.dest(`${dir.build}/`));
 });
 
+// Tarea encargada de preprosesar sass utilisando la extension scss.
 gulp.task('sass', ()=>{
     gulp.src(`${dir.src}/sass/*.scss`)
         .pipe(sass(options.sass).on('error', sass.logError))
@@ -69,16 +81,21 @@ gulp.task('sass', ()=>{
         .pipe(browserSync.stream());
 });
 
+// Tarea encargada de preprocesar JavaScript a ES5
 gulp.task('es6', ()=>{
     gulp.src(`${dir.src}/es6/*.js`)
         .pipe(babel(options.babel))
         .pipe(gulp.dest(`${dir.build}/js/`));
 });
 
+// Tarea encargada de crear e servidor de browserSync y preprocesar por si hubo un cambio previo
 gulp.task('serve', ['pug', 'sass', 'es6'], ()=> {
     browserSync.init(options.browsersync);
 });
 
+// Tarea por defecto de gulp encargada de ejecutar nuestras configuracines.
+// se puede pasar el parametro --env production de la siguiente manera
+// gulp --env production
 gulp.task('default', ()=> {
         gulp.start('serve');
     if (!IsProduction){
@@ -98,14 +115,17 @@ gulp.task('default', ()=> {
     }
 });
 
+// funcion encargada de recargar el navegador.
 function reload(){
     return browserSync.reload();
 }
 
+// funcion encargada de decirnos donde se realizo el cambio.
 function change(e){
     console.log('File ' + e.path + ' was ' + e.type + ', running tasks...');
 }
 
+// Funcion encargada de obtener el valor la linea de comandos si para saber si tiene que compilar en modo producción
 function getArg(key) {
     var index = process.argv.indexOf(key);
     var next = process.argv[index + 1];
